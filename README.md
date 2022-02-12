6e7eec403cde4c51f12d580defa2ed94c494bcbb
Juan Patrón <josejuanpatron1630@gmail.com>
2022-02-11 23:24:21 -0600

    Primeros comandos en SQL

---
+++ README.md
@@ -0,0 +1,4 @@
+# SQL
+
+## SQL es un lenguaje de consulta de datos, su nombre viene de la siglas en ingles (Strucutre Query Language)
+- Existen varios gestores de bases de dato como lo son SQL server, MySql, Oracle, etc.
---
+++ example.sql
@@ -0,0 +1,15 @@
+use cursoSql;
+
+/*
+	Comandos de seleccion simple
+*/
+select *  from CLIENTES;
+
+select * from PRODUCTOS;
+
+
+select empresa, poblaci_n  from CLIENTES;
+
+/*
+    Solo muestra los datos seleciconados
+*/
