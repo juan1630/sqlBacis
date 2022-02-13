eaa6579f8072c758f765d50951f1638810143b6e
Juan Patrón <josejuanpatron1630@gmail.com>
2022-02-13 11:10:53 -0600

    Consultas con la clausula WHERE, operadores logicos, operadores de comparacion

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 ## SQL es un lenguaje de consulta de datos, su nombre viene de la siglas en ingles (Strucutre Query Language)
 - Existen varios gestores de bases de dato como lo son SQL server, MySql, Oracle, etc.
+
+### COMANDOS DE SELECCION
+    
+    #### select
+    #### consultas con clausula WHERE 
+    #### Consultas con los operadores logicos 
+    #### Consultas con los operadores de comparacion
--- example.sql
+++ example.sql
@@ -6,10 +6,44 @@ use cursoSql;
 select *  from CLIENTES;
 
 select * from PRODUCTOS;
+/*
+    Solo muestra los datos seleciconados
+*/
 
 
 select empresa, poblaci_n  from CLIENTES;
 
+
 /*
-    Solo muestra los datos seleciconados
+    CONSULTA CON SENTENCIA WHERE
+    Ingresa un filtrado por el tipo de dato que se le pase
+*/
+
+SELECT  FECHA, SECCI_N, NOMBREART_CULO FROM PRODUCTOS WHERE SECCI_N='DEPORTES';
+
+/*
+    SENTENCIA WHERE CON OPERADORES LOGICOS
+    El operador OR responde a la pregunata Y, es decir quiero los items de una seccion y la otra
+    quiero ver los items de la seccion deportes y de la seccion ceramica
+*/
+
+SELECT  FECHA, SECCI_N, NOMBREART_CULO FROM PRODUCTOS WHERE SECCI_N='DEPORTES' OR SECCI_N='CONFECCI?N';
+
+
+
+SELECT * FROM PRODUCTOS WHERE SECCI_N='DEPORTES' AND PA_SDEORIGEN='USA';
+/*
+	Se usa el operador AND porque se quiere ver los articulos que sean unicamente de USA
+*/
+
+
+SELECT * FROM PRODUCTOS WHERE PRECIO=500000;
+/*
+	Trae los productos cuyo precio sea IGUAL a 500000
+*/
+
+
+SELECT * FROM PRODUCTOS WHERE PRECIO>=144;
+/*
+	Trae los productos cuyo precio sean MAYOR O IGUAL a 500000
 */
